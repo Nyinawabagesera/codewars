c555b22f1b848c12079348861b79cd16b5c0811f
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-11-19 08:27:10 +0200

    question5

---
+++ question4.js
@@ -0,0 +1,8 @@
+//Given the string representations of two integers, return the string
+ //representation of the sum of those integers.
+
+ function sumStrings(a,b) { 
+    let sum = parseInt(a)+parseInt(b)
+return String(sum)
+}
+console.log(sumStrings('1','2'))
